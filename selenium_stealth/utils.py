b40780466c530abd2f3ef0b09e83f1628e39661e
David <Gettippi@users.noreply.github.com>
2023-05-04 15:30:14 +0300

    commit 1

--- selenium_stealth/utils.py
+++ selenium_stealth/utils.py
@@ -4,6 +4,8 @@ from selenium.webdriver import Chrome as Driver
 
 
 def with_utils(driver: Driver, **kwargs) -> None:
+    print('path: ', Path(__file__))
+    print('parent: ', Path(__file__).parent)
     evaluateOnNewDocument(
         driver, Path(__file__).parent.joinpath("js/utils.js").read_text()
     )
