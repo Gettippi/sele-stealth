eaf337e0ded559e0fcfc5601259c01324973a7c0
David <Gettippi@users.noreply.github.com>
2023-05-04 15:54:22 +0300

    changed steup

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="selenium-stealth",
+    name="sele-stealth",
     version="1.0.6",
     author="Dipraj Patra",
     author_email="diprajpatra@gmail.com",
     description="Trying to make python selenium more stealthy.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/diprajpatra/selenium-stealth",
+    url="https://github.com/gettippi/sele-stealth",
     packages=setuptools.find_packages(),
-    package_data={"selenium-stealth": ["js/*.js"]},
+    package_data={"sele-stealth": ["js/*.js"]},
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
